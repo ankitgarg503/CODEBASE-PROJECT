1b9e57afe667c58e9d4dce08376bfbb4954826f5
Ankit Garg <ankitgarg3738@gmail.com>
2025-01-09 15:14:03 +0530

    Creating new listen api for Testing Prupose

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 // JavaScript Code
-console.log(s);
+const express =require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.get("/", (req, res) => {
+    res.send("Hii,I am root!");
+})
+
+const MONGO_URL ="mongodb://127.0.0.1:27017/test/AirBnb"
+
+main()
+    .then(()=>{
+        console.log("connected to db");
+    })
+    .catch((err) => {console.log("error: ", err);});
+    
+async function main(){
+    await mongoose.connect(MONGO_URL)
+}
+
+app.listen(8080,()=>{
+    console.log("listening on server 8080");
+});
