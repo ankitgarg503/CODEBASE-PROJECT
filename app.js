b231a41779859fdc4d103951f50dd8142c7ade5b
Ankit Garg <ankitgarg3738@gmail.com>
2025-01-11 14:52:37 +0530

    Connecting mongoDB and store one listing

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express =require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const Listing =require("./models/listing.js");
+
 app.get("/", (req, res) => {
     res.send("Hii,I am root!");
 })
 
-const MONGO_URL ="mongodb://127.0.0.1:27017/test/AirBnb"
+const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust"
 
 main()
     .then(()=>{
@@ -18,6 +20,20 @@ main()
 async function main(){
     await mongoose.connect(MONGO_URL)
 }
+
+app.get("/testListing", async (req, res) => {
+     let sampleListing =new Listing({
+        title:"My new Villa",
+        description:"By the beech",
+        price:1200,
+        location:"Calianguate,Goa",
+        country:"Inida",
+     });
+     await sampleListing.save();
+     console.log("sample was saved");
+     res.send("Succeful testing");
+});
+ 
 
 app.listen(8080,()=>{
     console.log("listening on server 8080");
