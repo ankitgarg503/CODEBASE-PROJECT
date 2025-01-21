9df336c4afd5ccb009cb1f9e86d566b317cfebb2
Ankit Garg <ankitgarg3738@gmail.com>
2025-01-21 15:10:53 +0530

    Adding New Route for Creating New Listing

--- app.js
+++ app.js
@@ -32,13 +32,31 @@ app.get("/listings", async (req, res) => {
    res.render("listings/index.ejs",{allListings});
 });
 
+// New Route
+app.get("/listings/new",(req,res) => {
+    res.render("listings/new.ejs");
+})
+
+
+
+
 // Show Route
 app.get("/listings/:id", async (req, res) => {
     let {id} =req.params;
    // Listing.findById(id);
     const listing =await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
+    res.render("listings/show.ejs",{ listing });
 });
+
+// Create Route
+app.post("/listings",async (req, res) => {
+    const newListing =new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+    // let listing =req.body;
+    // console.log(listing);
+})
+
 
 
 
